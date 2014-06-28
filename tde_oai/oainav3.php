ddb71be84d3533692f0ed4c27de6489c31d8964e
wtonribeiro <wtonribeiro@gmail.com>
2014-06-28 09:15:11 -0300

    Restringindo o branche aos arquivos do exportador

--- tde_oai/oainav3.php
+++
@@ -1,6 +0,0 @@
-<?php
-$CONTENT_TYPE = 'Content-type: text/xsl;';
-header($CONTENT_TYPE);
-header('Content-Disposition: attachment; filename="oainav3.xsl"');
-readfile('oainav3.xsl');
-?>
--- tde_oai/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Atualizando o protocolo OAI-PMH"
-echo "Aguarde..."
-svn export --force  http://tede-oai-pmh.googlecode.com/svn/trunk/ .
-echo "Atualizacao concluida"
